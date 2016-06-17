a39cf37d4bffa14e2bc6dacdc3f05923557568fc
Cameron <campoore2@gmail.com>
2016-06-17 08:47:24 -0700

    Added frontend function to allow for modal title and pizza value on 'Medium'. It will have a similar function for large.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@
 $(document).ready(function(){
   $("#smallPizza").click(function(){
     $("#modalTitle").text("");
-    $("#modalTitle").append("<h4><input type='hidden' id='pizzaPrice' value='2'>Small Pizza</input></h4>")
-  })
+    $("#modalTitle").append("<h4><input type='hidden' id='pizzaPrice' value='1'>Small Pizza</input></h4>");
+  });
+  $("#medPizza").click(function(){
+    $("#modalTitle").text("");
+    $("#modalTitle").append("<h4><input type='hidden' id='pizzaPrice' value='2'>Medium Pizza</input></h4>");
+  });
   event.preventDefault();
 })
